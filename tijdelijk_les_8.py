5d75e6219a12ed553d81f61e24cb604430c301c6
Wouter vd Zwan <w.a.vanderzwan@kpnmail.nl>
2023-07-20 11:46:27 +0200

    functies toegevoegd

---
+++ tijdelijk_les_8.py
@@ -0,0 +1,27 @@
+from helper import decoreer
+
+def print_aanbieding():
+    prijzen = {
+        "aardbei" : 3,
+        "vanille" : 4,
+        "chocolade" : 5
+    }
+
+    aanbieding = prijzen["aardbei"] * 0.8
+
+    reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
+
+    reclame_tekst2 = reclame_tekst[:63]
+
+    reclame_tekst3 = reclame_tekst2.upper()
+
+    reclame_tekst4 = reclame_tekst3.split()
+
+    for el in reclame_tekst4:
+        if len(el)>4:
+            print(el.upper())
+        else:
+            print(el.lower())
+
+decoreer("Aanbieding")
+print_aanbieding()
